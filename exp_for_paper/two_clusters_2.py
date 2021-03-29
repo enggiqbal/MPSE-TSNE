1b218c199ee5d0a5d3c9b8532532b6c7171e1414
enggiqbal <enggiqbal@gmail.com>
2021-03-28 19:00:35 -0700

    test

--- exp_for_paper/two_clusters_2.py
+++ exp_for_paper/two_clusters_2.py
@@ -5,7 +5,7 @@ import numpy as np
 #two clusters in 2 perspectives	200-2000 step 200	0.6*(# points)	2
 import pandas as pd 
 from tqdm import tqdm
-
+import math
 class Exp:
     def __init__(self):
         pass
@@ -13,11 +13,19 @@ class Exp:
         errors=[]
         for i in range(results.n_perspectives):
             Y = results.images[i]
+            
             labels = results.image_classes[i]
             error = mview.evaluate.separation_error(Y,labels )
             errors.append(error)
         return np.average(errors)
-    
+    def exp0(self):
+        n_clusters=2
+        n_sample=60 
+        for i in range (15):
+            results = mview.mpse_tsne('clusters', n_samples=n_sample, n_clusters=n_clusters, 
+            n_perspectives=2, perplexity=int(n_sample * 0.6),  show_plots=False, verbose=0)
+            print(results.images[0][0])
+
     def exp1(self, filename, n_perspectives=2 ):
         start=20
         end=200
@@ -26,12 +34,20 @@ class Exp:
         n_clusters=2
         final_results=[]
         for n_sample in tqdm(range( start, end, step)):
-            for i in range (repeat):
-                results = mview.mpse_tsne('clusters', n_samples=n_sample, n_clusters=n_clusters, n_perspectives=n_perspectives, perplexity=int(n_sample * 0.6),
-                                            show_plots=False, verbose=0)
-                e=self.get_error(results)
-                final_results.append([n_sample,i,n_perspectives,n_clusters,e, results.time ])
-                print(final_results[-1])
+            i=0
+            while(i<5):
+ 
+                results = mview.mpse_tsne('clusters',evaluate=False, n_samples=n_sample, n_clusters=n_clusters, n_perspectives=n_perspectives, perplexity=int(n_sample * 0.6),  show_plots=False, verbose=0)
+                print(results.images[0][0])
+                 
+                if math.isnan( results.images[0][0][0]) ==False:
+                    # e1, e2= results.image_separation
+                    e=0#(e1+e2)/n_perspectives
+                    final_results.append([n_sample,i,n_perspectives,n_clusters,e, results.time ])
+                    print(final_results[-1])
+                    i=i+1
+                else:
+                    print("scaping")
         df=pd.DataFrame(final_results, columns=['n_samples','exp_num','n_perspectives','n_clusters','separation_error','time'])
         df.to_csv(filename, index=False)
 
@@ -39,3 +55,4 @@ class Exp:
 if __name__=="__main__":
     e=Exp()
     e.exp1("two_clusters_2.csv")
+    # e.exp0()
