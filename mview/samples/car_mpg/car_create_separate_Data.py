28088d337a8a19e6ddbeb8d5a08744861c099231
Vahan Huroyan <vahanhuroyan@dhcp-10-142-198-128.uawifi.arizona.edu>
2021-08-14 19:00:35 -0700

    revised the ens-t-sne function for the car dataset

--- mview/samples/car_mpg/car_create_separate_Data.py
+++ mview/samples/car_mpg/car_create_separate_Data.py
@@ -4,9 +4,15 @@ import pandas as pd
 from scipy.sparse.csgraph import shortest_path
 import matplotlib.pyplot as plt
 
+# sys.path.insert(1,'../..')
+sys.path.insert(1,'/Users/vahanhuroyan/Documents/MPSE/MPSE-TSNE')
+
+import mview
+
+
 #file and pandas object with data:
 path = os.path.dirname(os.path.realpath(__file__))
-file = path+'/auto-mpg.csv'
+file = path+'/auto-mpg_mod.csv'
 
 print(file)
 
@@ -14,9 +20,44 @@ print(file)
 #     df = pd.read_csv(csvfile)
 # print(df)
 
-df1 = pd.read_csv(file, usecols = ['mpg',  'cylinders', 'displacement'])
-print(df1)
+# df1 = pd.read_csv(file)
+
+# df1 = pd.read_csv(file, usecols = ['mpg',  'cylinders', 'displacement'])
+# df2 = pd.read_csv(file, usecols = ['horsepower',  'weight', 'acceleration'])
+
+df1 = pd.read_csv(file, usecols=[0,1,2])
+
+df2 = pd.read_csv(file, usecols=[3,4,5])
+
+# df2["horsepower"] = [float(str(i).replace(",", "")) for i in df2["horsepower"]]
+
+# df1.to_csv(path + '/car_mpg_1.csv')
+
+a = df1.values
+
+b = df2.values
+
+# a.astype(float)
+# b.astype(float)
+
+
+print(len(a), len(b))
+print(len(a[0]), len(b[0]))
+print(a[0])
+print(b[0])
+
+
+
+# print(df1)
+
+# print([df1, df2])
 
 # pd.to_csv(df1, path + '1.csv')
 
-df1.to_csv(path + '/car_mpg_1.csv',  header = None, index=False)
+# df1.to_csv(path + '/car_mpg_1.csv',  header = None, index=False)
+
+print(type(a))
+print(type(b))
+
+data = [a, b]
+results = mview.mpse_tsne(data, perplexity=30, show_plots=True, verbose=2)
